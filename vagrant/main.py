6240132f41504717a2db4ccdd329641213230f48
VictorBischoff <morkebergv@gmail.com>
2023-01-09 05:54:38 +0100

    updated main

--- vagrant/main.py
+++ vagrant/main.py
@@ -1,70 +1,34 @@
-from ubuntu import start_ubuntu, stop_ubuntu
-from fedora import start_fedora, stop_fedora
-from debian import start_debian, stop_debian
+from debian import stop_debian, start_debian
+from ubuntu import stop_ubuntu, start_ubuntu
+from fedora import stop_fedora, start_fedora
+
+vm_functions = {
+    "ubuntu": {
+        "start": start_ubuntu,
+        "stop": stop_ubuntu
+    },
+    "fedora": {
+        "start": start_fedora,
+        "stop": stop_fedora
+    },
+    "debian": {
+        "start": start_debian,
+        "stop": stop_debian
+    }
+}
 
 while True:
-    print("FEDORA")
-    print("UBUNTU")
-    print("DEBIAN")
-    print("EXIT")
-    user_in = input("CHOOSE VM: ")
-    user_in = user_in.strip().lower()
+    vm = input("Choose a VM (ubuntu, fedora, debian, or exit): ").strip().lower()
     
-    if user_in == "ubuntu":
-        
-        print("START")
-        print("STOP")
-        print("EXIT")
-        ubuntu_user_in = input("CHOOSE OPTION:")
-        ubuntu_user_in = ubuntu_user_in.strip().lower()
-        
-        if ubuntu_user_in == "start":
-            start_ubuntu()
-            break
-        if ubuntu_user_in == "stop":
-            stop_ubuntu()
-            break
-        if ubuntu_user_in == "exit":
-            break
-    
-    if user_in == "fedora":
-        
-        print("START")
-        print("STOP")
-        print("EXIT")
-        fedora_user_in = input("CHOOSE OPTION:")
-        fedora_user_in = fedora_user_in.strip().lower()
-        
-        
-        if fedora_user_in == "start":
-            start_fedora()
-            break
-        if fedora_user_in == "stop":
-            stop_fedora()
-            break
-        if fedora_user_in == "exit":
-            break
-        
-    if user_in == "debian":
-        
-        print("START")
-        print("STOP")
-        print("EXIT")
-        debian_user_in = input("CHOOSE OPTION:")
-        debian_user_in = debian_user_in.strip().lower()
-        
-        
-        if debian_user_in == "start":
-            start_debian()
-            break
-        if debian_user_in == "stop":
-            stop_debian()
-            break
-        if debian_user_in == "exit":
-            break
-        
-    if user_in == "exit":
+    if vm == "exit":
         break
     
+    if vm in vm_functions:
+        action = input("Choose an action (start or stop): ").strip().lower()
+        
+        if action in vm_functions[vm]:
+            vm_functions[vm][action]()
+        else:
+            print("Invalid action")
     else:
-        print("Invalid action")
+        print("Invalid VM")
