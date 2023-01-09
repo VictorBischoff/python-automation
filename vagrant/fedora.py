faa6a8caf30a31a19514d5477610e0a3d4610567
VictorBischoff <morkebergv@gmail.com>
2023-01-09 04:37:00 +0100

    added env file

--- vagrant/fedora.py
+++ vagrant/fedora.py
@@ -1,13 +1,18 @@
 import vagrant
+import os
 from fabric.api import env
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 def start_fedora():
-    fedora_vm = vagrant.Vagrant("/Users/victormorkeberg/Documents/vms/fedora")
+    fedora_vm = vagrant.Vagrant(os.getenv("FEDORAPATH"))
     fedora_vm.up()
     env.hosts = [fedora_vm.user_hostname_port()]
     env.key_filename = fedora_vm.keyfile()
     env.disable_known_hosts = True
 
 def stop_fedora():
-    fedora_vm = vagrant.Vagrant("/Users/victormorkeberg/Documents/vms/fedora")
+    fedora_vm = vagrant.Vagrant(os.getenv("FEDORAPATH"))
     fedora_vm.halt()
--- vagrant/ubuntu.py
+++ vagrant/ubuntu.py
@@ -1,13 +1,17 @@
 import vagrant
+import os
 from fabric.api import env
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def start_ubuntu():
-    ubuntu_vm = vagrant.Vagrant("/Users/victormorkeberg/Documents/vms/ubuntu")
+    ubuntu_vm = vagrant.Vagrant(os.getenv("UBUNTUPATH"))
     ubuntu_vm.up()
     env.hosts = [ubuntu_vm.user_hostname_port()]
     env.key_filename = ubuntu_vm.keyfile()
     env.disable_known_hosts = True
 
 def stop_ubuntu():
-    ubuntu_vm = vagrant.Vagrant("/Users/victormorkeberg/Documents/vms/ubuntu")
+    ubuntu_vm = vagrant.Vagrant(os.getenv("UBUNTUPATH"))
     ubuntu_vm.halt()
